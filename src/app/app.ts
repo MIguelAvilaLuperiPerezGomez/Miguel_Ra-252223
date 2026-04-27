b95167528caa70146ad0eee766bcf261c67e8e3f
Miguel <migueltkd63@gmail.com>
2026-04-26 23:20:11 -0300

    versao inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,53 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { Tarefa } from "./tarefa";
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.css'
 })
-export class App {
+ 
+export class App implements OnInit {
   protected readonly title = signal('TODOapp');
+  apiURL: string;
+  Tarefas = signal<Tarefa[]>([]);  // ✅ Apenas o Signal
+ 
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
+  }
+ 
+  ngOnInit() {
+    this.READ_tarefas();
+  }
+ 
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); }
+    );
+  }
+ 
+  READ_tarefas() {
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+      resultado => this.Tarefas.set(resultado)  // ✅ .set() no Signal
+    );
+  }
+ 
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    const indice = this.Tarefas().indexOf(tarefaAserRemovida);
+    const id = this.Tarefas()[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); }
+    );
+  }
+ 
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    const indice = this.Tarefas().indexOf(tarefaAserModificada);
+    const id = this.Tarefas()[indice]._id;
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`, tarefaAserModificada).subscribe(
+      resultado => { console.log(resultado); this.READ_tarefas(); }
+    );
+  }
 }
